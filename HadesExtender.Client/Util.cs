4353a73d9e8b675518cf145f8d4ec520961a7f18
spacehamster <mail.spacehamster@gmail.com>
2020-10-29 12:03:56 +1100

    Fix Util.HookSafe from writing over the memory of previous hooks

--- HadesExtender.Client/Util.cs
+++ HadesExtender.Client/Util.cs
@@ -1,4 +1,4 @@
-﻿using Reloaded.Hooks;
+﻿using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.Enums;
 using Reloaded.Hooks.Tools;
 using System;
@@ -32,14 +32,13 @@ namespace HadesExtender
 
         //Reloaded.Hooks 2.4.0 underestimates the size of AsmHooks and can sometimes throw exceptions while trying to hook
         //Remove this once the bug is fixed in Reloaded.Hooks
-        public static AsmHook HookSafe(string[] asmCode, long functionAddress, AsmHookBehaviour behaviour = AsmHookBehaviour.ExecuteFirst)
+        public static IAsmHook HookSafe(string[] asmCode, long functionAddress, AsmHookBehaviour behaviour = AsmHookBehaviour.ExecuteFirst)
         {
             var asmBytes = Utilities.Assembler.Assemble(asmCode);
             var _is64Bit = IntPtr.Size == 8;
             int maxJmpSize = 7; // Maximum size of jmp opcode.
             var hookLength = Utilities.GetHookLength((IntPtr)functionAddress, maxJmpSize, _is64Bit);
-            hookLength += _is64Bit ? 24 : 12; //AsmHook under estimates the size of hooks by 3 pointers
-            return new AsmHook(asmBytes, functionAddress, behaviour, hookLength);
+            return new ExtenderAsmHook(asmBytes, functionAddress, behaviour, hookLength);
         }
 
         public static string ExtenderDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
