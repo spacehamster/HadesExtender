57c32d0a8d7e16790c159a9fbbdbcdf546f6bf09
spacehamster <mail.spacehamster@gmail.com>
2020-11-08 21:11:46 +1100

    Add filecache for mod and game scripts

--- LuaScripts/InitDebugging.lua
+++ LuaScripts/InitDebugging.lua
@@ -1,17 +1,38 @@
 ﻿local json = require 'cjson'
 local debuggee = require 'vscode-debuggee'
+local lfs = require 'lfs'
+local filepathCache = {}
 
-function ResolveChunkPath(fileName)
-	return "Content/Scripts/" .. fileName .. ".lua"
+local function BuildLookup(root, directory)
+	local currentDir = root .. "/" .. directory
+	for file in lfs.dir(currentDir) do
+		local filepath = currentDir .. '/' .. file
+		if file == "." or file == ".." then
+			--skip
+		elseif lfs.attributes(filepath, "mode") == "file" then
+			local chunkname = file:match("(.+)%..+")
+			if chunkname ~= nil and filepathCache[chunkname] == nil then
+				filepathCache[chunkname] = directory .. "/" .. file
+			end
+		elseif lfs.attributes(filepath, "mode") == "directory" then
+			BuildLookup(root, directory .. "/" .. file)
+		end
+	end
 end
 
+local function ResolveChunkPath(filename)
+	if filepathCache[filename] ~= nil then
+		return filepathCache[filename]
+	else
+		return filename
+	end
+end
+
+BuildLookup("..", "Content/Scripts")
+BuildLookup("..", "Content/Mods")
 local startResult, breakerType = debuggee.start(json, { resolveChunkPath = ResolveChunkPath })
 print('debuggee start ->', startResult, breakerType)
 
 function OnExtenderDebugUpdate()
 	debuggee.poll()
-end
-
-function abc()
-	DebugWriteLine("ABC", "DEF")
 end
