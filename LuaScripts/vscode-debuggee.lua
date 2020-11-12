7b959cc5dee7251450d404f561e7002bf78b3908
spacehamster <mail.spacehamster@gmail.com>
2020-11-12 21:55:14 +1100

    Fix debugger to properly step over and out of coroutines

--- LuaScripts/vscode-debuggee.lua
+++ LuaScripts/vscode-debuggee.lua
@@ -55,6 +55,16 @@ coroutine.create = function(f)
 	return c
 end
 
+local coroutineParents = {}
+
+local coresume = coroutine.resume
+coroutine.resume = function(co, ...)
+	coroutineParents[co] = coroutine.running()
+	local result = coresume(co, ...)
+	coroutineParents[co] = nil
+	return result
+end
+
 -------------------------------------------------------------------------------
 local function debug_getinfo(depth, what)
 	if debugTargetCo then
@@ -989,8 +999,26 @@ end
 
 -------------------------------------------------------------------------------
 local stepTargetHeight = nil
+local stepTargetThread = nil
+
+local function shouldBreak()
+	if stepTargetHeight == nil or stepTargetThread == nil then
+		return true
+	end
+	local currentThread = coroutine.running()
+	if coroutineParents[stepTargetThread] == currentThread then
+		stepTargetThread = currentThread
+		stepTargetHeight = stackHeight() - 2
+		return false
+	end
+	if stackHeight() - 1 <= stepTargetHeight and currentThread == stepTargetThread then
+		return true
+	end
+	return false
+end
+
 local function step()
-	if (stepTargetHeight == nil) or (stackHeight() <= stepTargetHeight) then
+	if shouldBreak() then
 		breaker.setLineBreak(nil)
 		baseDepth = breaker.stackOffset.stepDebugLoop
 		startDebugLoop()
@@ -1000,6 +1028,7 @@ end
 -------------------------------------------------------------------------------
 function handlers.next(req)
 	stepTargetHeight = stackHeight() - breaker.stackOffset.step
+	stepTargetThread = coroutine.running()
 	breaker.setLineBreak(step)
 	sendSuccess(req, {})
 	return 'CONTINUE'
@@ -1008,6 +1037,7 @@ end
 -------------------------------------------------------------------------------
 function handlers.stepIn(req)
 	stepTargetHeight = nil
+	stepTargetThread = nil
 	breaker.setLineBreak(step)
 	sendSuccess(req, {})
 	return 'CONTINUE'
@@ -1016,6 +1046,7 @@ end
 -------------------------------------------------------------------------------
 function handlers.stepOut(req)
 	stepTargetHeight = stackHeight() - (breaker.stackOffset.step + 1)
+	stepTargetThread = coroutine.running()
 	breaker.setLineBreak(step)
 	sendSuccess(req, {})
 	return 'CONTINUE'
